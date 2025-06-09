529abaa9a6b6a47eeb59b9e919a31adc7689f48a
hakoya <tsukusister2@gmail.com>
2025-06-09 22:38:25 +0900

    database.ymlの編集4

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -79,6 +79,9 @@ Rails.application.configure do
   # Only use :id for inspections in production.
   config.active_record.attributes_for_inspect = [ :id ]
 
+  # config/environments/production.rb
+  config.action_cable.mount_path = nil
+
   # Enable DNS rebinding protection and other `Host` header attacks.
   # config.hosts = [
   #   "example.com",     # Allow requests from example.com
